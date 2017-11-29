47294c7ae3ceed82c7b1b0b4eef4a38881571c7c
Mateusnunes <mateununes@me.com>
2017-11-28 23:29:59 -0300

    incrementando classe pontos

--- scripts/ponto3D.js
+++ scripts/ponto3D.js
@@ -4,8 +4,18 @@ function Ponto3D(x, y, z){
     //construtor
     this.x = x;
     this.y = y;
-    this.z = z;
-    
-    
-    
+    this.z = z;    
+}
+
+Ponto3D.prototype.sub = function(point){
+    return new Ponto3D ( this.x - point.x, this.y - point.y, this.z - point.z);
+}
+
+Ponto3D.prototype.add = function(point){
+    return new Ponto3D ( this.x + point.x, this.y + point.y, this.z + point.z);
+}
+
+
+Ponto3D.prototype.mult = function(number){
+    return new Ponto3D ( this.x * number , this.y * number, this.z * number.z);
 }
