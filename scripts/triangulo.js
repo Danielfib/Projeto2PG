4b8db30b73643fc648eb571d6ddc3bd9e62379b2
Mateusnunes <mateununes@me.com>
2017-11-28 22:39:04 -0300

    criando triangulos e adicionando construtor, bem ocmo calculo da norma

--- scripts/triangulo.js
+++ scripts/triangulo.js
@@ -1,4 +1,4 @@
-function triangulo(a,b,c){
+function Triangulo(a,b,c){
     //vamos começar cosntruindo o construtor básico
     this.a = c; 
     this.b = b ; 
@@ -6,5 +6,10 @@ function triangulo(a,b,c){
 
     this.normal = new Vetor(0,0,0);
 
-
+    Triangulo.prototype.normal = function(){
+        var v = new Vetor(this.b.x - this.a.x, this.b.y - this.a.y, this.b.z - this.a.z );
+        var u = new Vetor(this.c.x - this.b.x, this.c.y - this.b.y, this.c.z - this.b.z );
+        this.normal = v.produtoVetorial(u);
+        this.normal.normaliza();
+    }
 }
