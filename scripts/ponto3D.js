3ca1260b795204b38df1c50f66dd60dfd420848d
Mateusnunes <mateununes@me.com>
2017-12-21 17:46:15 -0300

    colocando p1 do checklist 2 no ponto3d

--- scripts/ponto3D.js
+++ scripts/ponto3D.js
@@ -1,3 +1,5 @@
+var pontosTela= [];
+
 function Ponto3D(x, y, z){
     //construtor
     this.x = x
@@ -22,4 +24,23 @@ Ponto3D.prototype.mult = function(k){
 
 Ponto3D.prototype.clone = function() {
     return new Ponto3D(this.x, this.y, this.z);
+}
+
+Ponto3D.prototype.transfCoordenadasTela= function() {
+    // a linha abaixo gera os pontos 2D parametrizados no intervalo [-1, 1]:
+    var dhx = Camera.d / Camera.hx;
+    var dhy = Camera.d / Camera.hy;
+    var novoX = dhx * ( ponto3D.x / ponto3D.z );
+    var novoY = dhy * ( ponto3D.y / ponto3D.z );
+    // em seguida parametrizamos os pontos para as dimensões da janela (intervalos [0, width] e [0, height]) ,
+    // transformando tudo em inteiro, podendo descartar os pontos gerados no intervalo [-1, 1].
+    novoX = parseInt((novoX+1) * (canvas.width/2) );
+    novoY = parseInt((1-novoY)* (canvas.height/2) );
+    return new ponto2D(novoX,novoY);
+}
+
+Ponto3D.prototype.convertendoTodosPonto = function() {
+    for( var i = 0 ; i < object.pontos.lenght ; i++) {
+        pontosTela[i] = object.pontos[i].TransfCoordenadasTela;
+    }
 }
